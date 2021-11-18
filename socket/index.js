7a7db0956be8532bbfcd4ae29b477c7c0fd47733
Bagus <sgiapi1@gmail.com>
2021-11-18 11:33:39 +0700

    upload firs project

---
+++ socket/index.js
@@ -0,0 +1,50 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors: {
+        origin:"http://localhost:3000"
+    }
+});
+
+let onlineUser = [];
+
+const addNewUser = (username, socketId) => {
+    !onlineUser.some((user) => user.username === username) && onlineUser.push({username,socketId});
+}
+
+const removeUser = (socketId) => {
+    onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+}
+
+const getUser = (username) => {
+    return onlineUser.find((user) => user.username === username)
+}
+
+io.on("connection", (socket) => {
+
+    socket.on("newUser", (username) => {
+        addNewUser(username, socket.id);
+    });
+
+    socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+        const receiver = getUser(receiverName);
+        io.to(receiver.socketId).emit("getNotification", {
+            senderName,
+            type
+        })
+    });
+
+    // socket.io("sendText", ({ senderName, receiverName, text }) => {
+    //     const receiver = getUser(receiverName);
+    //     io.to(receiver.socketId).emit("getNotification", {
+    //         senderName,
+    //         text,
+    //     })
+    // })
+
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
+    })
+});
+
+io.listen(5000);
